25a35d6e91fca8e59ae9345c5288257c5e07cb0e
Olha <orukshyna@gmail.com>
2021-08-12 18:26:33 +0200

    Modified according flake8

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -18,20 +18,19 @@ class Group(models.Model):
 
 
 class Post(models.Model):
-    text = models.TextField(blank=False, verbose_name='Текст')
-    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='День публикации')
+    text = models.TextField(blank=False)
+    pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts',
-    )
+        related_name='posts')
+
     group = models.ForeignKey(
         Group,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name='posts',
-        )
+        related_name='posts')
 
     class Meta:
         ordering = ['-pub_date']
